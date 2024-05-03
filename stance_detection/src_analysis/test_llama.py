7cd425cc032e9ac7ef00498702c6882686a40434
Tyrion Huu <tyrionhuu@gmail.com>
2024-05-03 12:51:02 +1000

    a

--- stance_detection/src_analysis/test_llama.py
+++ stance_detection/src_analysis/test_llama.py
@@ -1,8 +1,8 @@
 from llama_cpp import Llama
 
-my_model_path = '../models/codellama-7b.Q5_K_M.gguf'
+my_model_path = '../models/Meta-Llama-3-8B-Instruct.Q8_0.gguf'
 CONTEXT_SIZE = 512
 
 model = Llama(model_path=my_model_path, n_ctx=CONTEXT_SIZE)
 
-print(model("The quick brown fox jumps ", stop=["."])["choices"][0])
+print(model("The quick brown fox jumps ", stop=["."])["choices"][0].text)
