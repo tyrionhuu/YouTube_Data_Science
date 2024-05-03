865bb5862bfae1eb4cf2680fb282597191a6c8d9
Tyrion Huu <tyrionhuu@gmail.com>
2024-05-03 15:24:00 +1000

    A

--- stance_detection/src_analysis/test_llama.py
+++ stance_detection/src_analysis/test_llama.py
@@ -9,7 +9,6 @@ output = model(
       "Q: Name the planets in the solar system? A: ", # Prompt
       max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
       stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
-      echo=True
 ) # Generate a completion, can also call create_completion
 
 print(output) # Print the generated completion
